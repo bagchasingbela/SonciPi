34b9494f48c6b0526eb456a7bf4352d658452e71
bagchasingbela <124717187+bagchasingbela@users.noreply.github.com>
2023-02-17 11:39:40 -0800

    Update StrangerThings.rb

--- StrangerThings.rb
+++ StrangerThings.rb
@@ -1,1 +1,18 @@
+play :c2
+sleep 0.5
+play :e2
+sleep 0.5
+play :g2
+sleep 0.5
+play :b2
+sleep 0.5
 
+play :c3
+sleep 0.5
+play :b2
+sleep 0.5
+play :g2
+sleep 0.5
+play :e2
+sleep 0.5
+#havent finshed because i was absent.
