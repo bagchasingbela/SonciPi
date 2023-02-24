792a5b094af5f9619e2b841c9caa017a60d108db
bagchasingbela <124717187+bagchasingbela@users.noreply.github.com>
2023-02-24 11:37:40 -0800

    Update CustomSong.rb

--- CustomSong.rb
+++ CustomSong.rb
@@ -1,71 +1,184 @@
 # Customsong
 use_bpm 120
 use_synth :piano
-live_loop :middle do
-  1.times do
-    wait 1
-    play:c5
-    play:c6
-    sleep 1
-    wait 1
-    play:a4
-    play:a5
-    
-    wait 1
-    play:e4
-    play:e5
-    sleep 1
-    wait 1
-    play:e4
-    play:e5
-    
-    wait 1
-    play:c5
-    play:c6
-    sleep 1
-    wait 1
-    play:a4
-    play:a5
-    
-    wait 1
-    play:e4
-    play:e5
-    sleep 1
-    wait 1
-    play:e4
-    play:e5
+define :oneandtwo do
+  live_loop :middle do
+    2.times do
+      wait 1
+      play:c5
+      play:c6
+      sleep 1
+      wait 1
+      play:a4
+      play:a5
+      sleep 1
+      
+      wait 1
+      play:e4
+      play:e5
+      sleep 1
+      wait 1
+      play:e4
+      play:e5
+      sleep 1
+      
+      wait 1
+      play:c5
+      play:c6
+      sleep 1
+      wait 1
+      play:a4
+      play:a5
+      sleep 1
+      
+      wait 1
+      play:e4
+      play:e5
+      sleep 1
+      wait 1
+      play:e4
+      play:e5
+      sleep 1
+    end
+    stop
   end
-  stop
+  
+  live_loop :end do
+    2.times do
+      play:c4
+      sleep 2
+      play:a3
+      sleep 2
+      
+      play:e3
+      sleep 2
+      play:e3
+      sleep 2
+      
+      play:c4
+      sleep 2
+      play:a3
+      sleep 2
+      
+      play:e3
+      sleep 2
+      play:e3
+      sleep 2
+    end
+    stop
+  end
+  
+  live_loop :first do
+    2.times do
+      sleep 10
+      play:e4
+      sleep 2
+      
+      play:e4
+      sleep 2
+      play:e4
+      sleep 0.5
+      play:d4
+      sleep 0.5
+      wait 1
+    end
+    stop
+  end
+  live_loop :middle2 do
+    sleep 12
+    1.times do
+      wait 1
+      play:c4
+      play:c5
+      sleep 1
+      wait 1
+      play:f4
+      play:f5
+      sleep 1
+      play:a3
+      play:a4
+      sleep 1
+      wait 1
+      play:a3
+      play:a4
+      sleep 1
+      wait 1
+      wait 1
+      play:c4
+      play:c5
+      sleep 1
+      wait 1
+      play:f4
+      play:f5
+      sleep 1
+      play:a3
+      play:a4
+      sleep 1
+      wait 1
+      play:a3
+      play:a4
+      sleep 1
+      wait 1
+    end
+    stop
+  end
+  
+  live_loop :end2 do
+    sleep 12
+    1.times do
+      play:b3
+      sleep 2
+      play:f3
+      sleep 2
+      play:a2
+      sleep 2
+      play:a2
+      sleep 2
+      play:b3
+      sleep 2
+      play:f3
+      sleep 2
+      play:a2
+      sleep 2
+      play:a2
+      sleep 2
+    end
+    stop
+  end
+  
+  live_loop :first2 do
+    sleep 12
+    1.times do
+      sleep 4
+      wait 1
+      wait 0.5
+      play:e4
+      sleep 0.25
+      play:e4
+      sleep 0.25
+      play:e4
+      sleep 0.5
+      play:d4
+      sleep 0.5
+      sleep 1
+      wait 1
+      wait 0.5
+      play:e4
+      sleep 0.25
+      play:e4
+      sleep 0.25
+      play:e4
+      sleep 0.5
+      play:d4
+      sleep 0.5
+      wait 1
+      wait 4
+      
+    end
+    stop
+  end
 end
 
-live_loop :end do
-  1.times do
-    play:c4
-    sleep 2
-    play:a3
-    sleep 2
-    
-    play:e3
-    sleep 2
-    play:e3
-    sleep 2
-    
-    play:c4
-    sleep 2
-    play:a3
-    sleep 2
-    
-    play:e3
-    sleep 2
-    play:e3
-    sleep 2
-  end
-  stop
-end
-
-live_loop :first do
-  1.times do
-    play:c4
-  end
-  stop
+1.times do
+  oneandtwo
 end
