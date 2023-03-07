a9de4a60648e2b3baf790d8870ec6768fc880940
bagchasingbela <124717187+bagchasingbela@users.noreply.github.com>
2023-03-07 10:14:18 -0800

    Update CustomSong.rb

--- CustomSong.rb
+++ CustomSong.rb
@@ -1,82 +1,104 @@
+define:red do
+  wait 1
+  play:c5
+  play:c6
+  sleep 1
+  wait 1
+  play:a4
+  play:a5
+  sleep 1
+end
+define:blue do
+  wait 1
+  play:e4
+  play:e5
+  sleep 1
+  wait 1
+  play:e4
+  play:e5
+  sleep 1
+end
+define:green do
+  play:c4
+  sleep 2
+  play:a3
+  sleep 2
+end
+define:lilac do
+  play:e3
+  sleep 2
+  play:e3
+  sleep 2
+end
+define:lime do
+  play:c3
+  sleep 2
+  play:f3
+  sleep 2
+end
+define:yellow do
+  wait 1
+  wait 0.5
+  play:e4
+  sleep 0.25
+  play:e4
+  sleep 0.25
+  play:e4
+  sleep 0.5
+  play:d4
+  sleep 0.5
+  wait 1
+end
+define:purple do
+  wait 1
+  play:c3
+  play:c4
+  sleep 1
+  wait 1
+  play:f3
+  play:f4
+  sleep 1
+end
+define:maroon do
+  play:a2
+  play:a3
+  sleep 1
+  wait 1
+  play:a2
+  play:a3
+  sleep 1
+  wait 1
+end
 use_bpm 120
 use_synth :tech_saws
 live_loop :middle do
   2.times do
-    with_fx :wobble ,smooth:5,amp:1 do
-      wait 1
-      play:c5
-      play:c6
-      sleep 1
-      wait 1
-      play:a4
-      play:a5
-      sleep 1
-      
-      wait 1
-      play:e4
-      play:e5
-      sleep 1
-      wait 1
-      play:e4
-      play:e5
-      sleep 1
-      
-      wait 1
-      play:c5
-      play:c6
-      sleep 1
-      wait 1
-      play:a4
-      play:a5
-      sleep 1
-      
-      wait 1
-      play:e4
-      play:e5
-      sleep 1
-      wait 1
-      play:e4
-      play:e5
-      sleep 1
+    with_fx :wobble ,smooth:4,amp:1,mix:1 do
+      red
+      blue
+      red
+      blue
     end
   end
   stop
 end
-
 live_loop :end do
   2.times do
-    with_fx :wobble ,smooth:5,amp:1 do
-      play:c4
-      sleep 2
-      play:a3
-      sleep 2
-      
-      play:e3
-      sleep 2
-      play:e3
-      sleep 2
-      
-      play:c4
-      sleep 2
-      play:a3
-      sleep 2
-      
-      play:e3
-      sleep 2
-      play:e3
-      sleep 2
+    with_fx :wobble ,smooth:4,amp:1,mix:1 do
+      green
+      lilac
+      green
+      lilac
     end
   end
   stop
 end
-
 live_loop :first do
   2.times do
-    with_fx :wobble ,smooth:5,amp:1 do
+    with_fx :wobble ,smooth:4,amp:1,mix:1 do
       sleep 10
       play:e4
       sleep 2
-      
       play:e4
       sleep 2
       play:e4
@@ -90,51 +112,26 @@ live_loop :first do
 end
 live_loop:first2 do
   1.times do
-    with_fx :wobble ,smooth:5,amp:1 do
+    with_fx :wobble ,smooth:6,amp:1,mix:1 do
       wait 32
       wait 4
-      wait 1
-      wait 0.5
-      play:e4
-      sleep 0.25
-      play:e4
-      sleep 0.25
-      play:e4
-      sleep 0.5
-      play:d4
-      sleep 0.5
-      wait 1
-      wait 1
-      wait 0.5
-      play:e4
-      sleep 0.25
-      play:e4
-      sleep 0.25
-      play:e4
-      sleep 0.5
-      play:d4
-      sleep 0.5
-      wait 1
+      yellow
+      yellow
+      wait 4
     end
   end
   stop
 end
 live_loop:last2 do
   1.times do
-    with_fx :wobble ,smooth:5,amp:1 do
+    with_fx :wobble ,smooth:4,amp:1,mix:1 do
       wait 32
-      play:c3
-      sleep 2
-      play:f3
-      sleep 2
+      lime
       play:a2
       sleep 2
       play:a2
       sleep 2
-      play:c3
-      sleep 2
-      play:f3
-      sleep 2
+      lime
       play:a2
       sleep 2
       wait 2
@@ -142,43 +139,14 @@ live_loop:last2 do
   end
   stop
 end
-
 live_loop:middle2 do
   1.times do
-    with_fx :wobble ,smooth:5,amp:1 do
+    with_fx :wobble ,smooth:4,amp:1,mix:1 do
       wait 32
-      wait 1
-      play:c4
-      play:c5
-      sleep 1
-      wait 1
-      play:f4
-      play:f5
-      sleep 1
-      play:a4
-      play:a3
-      sleep 1
-      wait 1
-      play:a4
-      play:a3
-      sleep 1
-      wait 1
-      wait 1
-      play:c4
-      play:c5
-      sleep 1
-      wait 1
-      play:f4
-      play:f5
-      sleep 1
-      play:a4
-      play:a3
-      sleep 1
-      wait 1
-      play:a4
-      play:a3
-      sleep 1
-      wait 1
+      purple
+      maroon
+      purple
+      maroon
     end
   end
   stop
