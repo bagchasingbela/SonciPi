f39c439cf01e5df8107705064fe76be1b2a0d7be
bagchasingbela <124717187+bagchasingbela@users.noreply.github.com>
2023-04-18 11:36:21 -0700

    Update CustomSongPt2.rb

--- CustomSongPt2.rb
+++ CustomSongPt2.rb
@@ -91,6 +91,16 @@ define:smile do |smileone|
   play smileone
   sleep 0.5
 end
+define:triangle do |try|
+  play:bb4
+  sleep 2
+  play:bb2
+  sleep 0.5
+  play:c3
+  sleep 1
+  play try
+  sleep 0.5
+end
 define:star do |starone,startwo|
   play starone
   sleep 2
@@ -139,13 +149,6 @@ define:pinkie do
   play:ab4
   sleep 0.5
 end
-
-
-
-
-
-
-
 onlyraysleep=[0.5,0.25,0.25,1,0.5,0.5,0.5,0.5]
 ors=0
 onlyraynotes=[:ab4,:ab4,:ab4,:ab4,:r,:ab4,:ab4,:ab4,]
@@ -327,4 +330,217 @@ live_loop:threeblockrd do
   star :db3,:db3
   stop
 end
-
+live_loop:fourblockst do
+  sleep 25.5
+  play:c5
+  sleep 0.75
+  play:bb4
+  sleep 0.25
+  play:bb4
+  sleep 1
+  play:r
+  sleep 0.5
+  play:ab4
+  sleep 0.5
+  play:ab4
+  sleep 0.5
+  play:ab4
+  sleep 0.5
+  play:bb4
+  sleep 0.25
+  play:f4
+  sleep 0.25
+  play:f4
+  sleep 0.25
+  play:f4
+  sleep 0.25
+  play:f4
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:bb4
+  sleep 0.25
+  play:ab4
+  sleep 0.25
+  play:ab4
+  sleep 0.5
+  stop
+end
+live_loop:fourblocknd do
+  use_synth :piano
+  sleep 25.5
+  play:c5
+  sleep 0.75
+  play:bb4
+  sleep 0.25
+  play:bb4
+  sleep 1
+  play:bb4
+  sleep 0.5
+  play:ab4
+  play:eb4
+  play:bb3
+  sleep 0.5
+  play:ab4
+  sleep 0.5
+  play:ab4
+  play:f4
+  play:c4
+  sleep 0.5
+  play:bb4
+  sleep 0.5
+  play:f4
+  sleep 0.25
+  play:f4
+  sleep 0.25
+  play:f4
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  play:ab4
+  play:f4
+  sleep 0.5
+  play:bb4
+  sleep 0.25
+  play:ab4
+  sleep 0.25
+  play:ab4
+  sleep 0.5
+  stop
+end
+live_loop:fourblockndtwo do
+  use_synth :piano
+  sleep 25.5
+  fd
+  play:r
+  sleep 2
+  fc
+  play:r
+  sleep 2
+  stop
+end
+live_loop:fourblockrd do
+  use_synth :piano
+  sleep 25.5
+  smile :f2
+  star :f2,:f2
+  stop
+end
+live_loop:fiveblockst do
+  sleep 33.5
+  play:ab5
+  sleep 4
+  play:ab5
+  sleep 0.5
+  play:g5
+  sleep 0.5
+  play:f5
+  sleep 0.5
+  play:eb5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:f5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:bb4
+  sleep 0.25
+  play:c5
+  sleep 0.25
+  play:bb4
+  sleep 0.25
+  play:ab4
+  sleep 2.75
+  stop
+end
+live_loop:fiveblocknd do
+  use_synth :piano
+  sleep 33.5
+  play:ab5
+  play:f5
+  play:db5
+  sleep 2
+  play:f5
+  play:db5
+  sleep 0.5
+  play:eb5
+  play:bb4
+  sleep 1
+  play:f5
+  play:c5
+  sleep 0.5
+  play:ab5
+  sleep 0.5
+  play:g5
+  sleep 0.5
+  play:f5
+  sleep 0.5
+  play:eb5
+  sleep 0.5
+  play:c5
+  sleep 0.5
+  play:c5
+  play:ab4
+  play:f4
+  sleep 0.5
+  play:f5
+  sleep 1
+  play:c5
+  sleep 0.5
+  play:bb4
+  sleep 0.5
+  play:ab4
+  sleep 1
+  play:ab4
+  play:f4
+  play:db4
+  sleep 0.5
+  play:ab4
+  play:g4
+  play:db4
+  sleep 1
+  play:ab4
+  play:f4
+  play:c4
+  sleep 0.5
+  stop
+end
+live_loop:fiveblockndtwo do
+  use_synth :piano
+  sleep 33.5
+  play:ab5
+  sleep 4
+  play:r
+  sleep 4
+  fd
+  play:r
+  sleep 2
+  stop
+end
+live_loop:fiveblockndtwo do
+  use_synth :piano
+  sleep 33.5
+  triangle :db3
+  play:db3
+  sleep 2
+  play:db3
+  sleep 0.5
+  play:db3
+  sleep 0.5
+  play:r
+  sleep 1
+  triangle :f2
+  stop
+end
